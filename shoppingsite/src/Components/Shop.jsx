10f04dd5ad7acad0050d51639c37b385a00e3b97
kc <sc571861@gmail.com>
2024-07-27 22:29:43 +0530

    Button completed

--- shoppingsite/src/Components/Shop.jsx
+++ shoppingsite/src/Components/Shop.jsx
@@ -1,11 +1,37 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 import NavBar from './NavBar'
 
 const Shop = () => {
+  const [products, setProducts] = useState([])
+  useEffect(() => {
+    async function fetchProducts() {
+      const response=await fetch('https://fakestoreapi.com/products');
+      const data=await response.json();
+      setProducts(data);
+        
+    }
+    fetchProducts()
+  }, [])
   return (
     <>
     <NavBar/>
-        <h1>Shop</h1>
+    <main>
+      <h1 className='title'>Shop</h1>
+      <div className='products'>
+      {products.map(product => (
+        <div className='product' key={product.id}>
+        <img className='product-img' src={product.image} alt={product.title}></img>
+        <h4 className='product-name'>{product.title}</h4>
+        <span className='product-price'>{product.price} $</span>
+        <div className='button'>
+          <button>Add To Cart</button>
+        </div>
+
+        </div>
+      ))}
+
+      </div>
+    </main>
     </>
   )
 }
